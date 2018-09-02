d928ca9a35394b3a3d8cc5b7012211e2d1b5679b
Andres Orejuela <andresforejuelab@gmail.com>
2018-09-02 03:45:20 -0500

    Creación de la aplicación y modelo Posts, Estructura API basica.

--- django_and_react/settings.py
+++ django_and_react/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # External apps
+    'rest_framework',
+
+    # Personal apps
+    'posts',
 ]
 
 MIDDLEWARE = [
---
+++ posts/views.py
@@ -0,0 +1,11 @@
+# Local Django
+from posts.models import Post
+from posts.serializers import PostSerializer
+
+# External libraries
+from rest_framework import generics
+
+class PostListCreate(generics.ListCreateAPIView):
+    """Using generic view."""
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
