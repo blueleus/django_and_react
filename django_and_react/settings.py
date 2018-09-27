22f69c4b37716101922fef27011b3f65af009e53
Andres Orejuela <andresforejuelab@gmail.com>
2018-09-26 23:24:34 -0500

    Implementación de React con Django REST.

--- django_and_react/settings.py
+++ django_and_react/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # Personal apps
     'posts',
+    'frontend',
 ]
 
 MIDDLEWARE = [
--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from posts.serializers import PostSerializer
 # External libraries
 from rest_framework import generics
 
+
 class PostListCreate(generics.ListCreateAPIView):
     """Using generic view."""
     queryset = Post.objects.all()
